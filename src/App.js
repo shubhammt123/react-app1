42b0e8a0bc50c743eb288d1eca99d8ebf15f7b7a
Shubham Jain <shubhammt123@gmail.com>
2023-03-30 12:46:38 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [counter,setCounter] = useState(0);
-
-  let msg;
-    if(counter%2==0){
-        msg = "even"
-    }else{
-        msg = "odd"
+  const [msg,setMsg] = useState('even');
+  const [value,setValue] = useState("please enter here");
+  useEffect(()=>{
+    if(value===""){
+      setValue("please enter here")
     }
+  },[value])
+
+  const onInputHandler = (event)=>{
+    let val = event.target.value;
+    setValue(val)
+    
+  }
+
+useEffect(()=>{
+  if(counter%2===0){
+    setMsg("even")
+}else{
+    setMsg("odd")
+}
+},[counter])
+    
+    
   return (
     <div className="App">
      
@@ -18,6 +33,11 @@ function App() {
      <p>{counter}</p>
                   <button disabled={counter === 0} onClick={()=> setCounter(counter-1)}>dec</button>
                   <button onClick={()=> setCounter(counter+1)}>inc</button>
+
+                  <div>
+                    <input onChange={onInputHandler} type="text" name="" id="" placeholder='type here' />
+                    <p>{value}</p>
+                  </div>
     </div>
   );
 }
